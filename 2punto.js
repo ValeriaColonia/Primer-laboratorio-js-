5738deaffe664fe0af26b946d8898d74a858a33e
ValeriaColonia <valeriaserna027@gmail.com>
2025-07-10 16:51:49 -0500

    ajuste

--- 2punto.js
+++ 2punto.js
@@ -8,3 +8,11 @@ año = prompt("¿En que año nació?: ")
 let edad = añoactual - año; 
 console.log("Usted tiene: ",edad, "años") 
 
+// suma dos numeros
+let num1; 
+num1 = prompt("Ingrese un número: ");
+let num2; 
+num2 = prompt("Ingrese otro número: ")
+
+
+
